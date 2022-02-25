f511c16ed5619212dead36bf4fb9911ae0c5091d
Matt Salmons <mattcsalmons@gmail.com>
2022-02-25 00:06:32 -0700

    footer complete

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Contact' />
+      <h1 className='headtext__cormorant' style={{ marginBottom: '3rem' }}>Find Us</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>750 Milford Street | Fryeburg, NH | 04037 </p>
+        <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0' }}>Hours</p>
+        <p className='p__opensans'>Mon - Fri: 11am - 10pm</p>
+        <p className='p__opensans'>Sat - Sun: 11am - midnight</p>
+      </div>
+      <button className='custom__button' style={{ marginTop: '2rem'}}>Visit Us</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt='find us'/>
+    </div>
   </div>
 );
 
